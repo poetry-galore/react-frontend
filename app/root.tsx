b07f1b6b6a90331812d64af568abf52be9b46ff1
Brian Kim <bryo.kim1@gmail.com>
2024-07-07 18:54:37 +0300

    Revert "Initialize shadcn ui and create ThemeProvider"
    This reverts commit 43737a1344175dd7da9539ef43e30586509100dc.

--- app/root.tsx
+++ app/root.tsx
@@ -1,45 +1,23 @@
-import clsx from "clsx";
-import {
-  PreventFlashOnWrongTheme,
-  ThemeProvider,
-  useTheme,
-} from "remix-themes";
-import { LoaderFunctionArgs } from "@remix-run/node";
 import {
   Links,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
-  useLoaderData,
 } from "@remix-run/react";
-
-import { themeSessionResolver } from "./theme/session.server";
 import "./tailwind.css";
 
-export async function loader({ request }: LoaderFunctionArgs) {
-  const { getTheme } = await themeSessionResolver(request);
-
-  return {
-    theme: getTheme(),
-  };
-}
-
-export function App() {
-  const data = useLoaderData<typeof loader>();
-  const [theme] = useTheme();
-
+export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className={clsx(theme)}>
+    <html lang="en">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
-        <PreventFlashOnWrongTheme ssrTheme={Boolean(data.theme)} />
         <Links />
       </head>
-      <body className="text-slate-800 bg-white dark:text-slate-100 dark:bg-gray-900 h-dvh">
-        <Outlet />
+      <body>
+        {children}
         <ScrollRestoration />
         <Scripts />
       </body>
@@ -47,15 +25,6 @@ export function App() {
   );
 }
 
-/**
- * App wrapped by the ThemeProvider
- */
-export default function AppWithThemeProvider() {
-  const data = useLoaderData<typeof loader>();
-
-  return (
-    <ThemeProvider specifiedTheme={data.theme} themeAction="/action/set-theme">
-      <App />
-    </ThemeProvider>
-  );
+export default function App() {
+  return <Outlet />;
 }
--- app/routes/action.set-theme.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Action for toggling the theme
- */
-
-import { createThemeAction } from "remix-themes";
-
-import { themeSessionResolver } from "~/theme/session.server";
-
-export const action = createThemeAction(themeSessionResolver);
