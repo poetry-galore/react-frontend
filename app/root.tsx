43737a1344175dd7da9539ef43e30586509100dc
Brian Kim <bryo.kim1@gmail.com>
2024-07-07 18:50:03 +0300

    Initialize shadcn ui and create ThemeProvider

--- app/root.tsx
+++ app/root.tsx
@@ -1,23 +1,45 @@
+import clsx from "clsx";
+import {
+  PreventFlashOnWrongTheme,
+  ThemeProvider,
+  useTheme,
+} from "remix-themes";
+import { LoaderFunctionArgs } from "@remix-run/node";
 import {
   Links,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from "@remix-run/react";
+
+import { themeSessionResolver } from "./theme/session.server";
 import "./tailwind.css";
 
-export function Layout({ children }: { children: React.ReactNode }) {
+export async function loader({ request }: LoaderFunctionArgs) {
+  const { getTheme } = await themeSessionResolver(request);
+
+  return {
+    theme: getTheme(),
+  };
+}
+
+export function App() {
+  const data = useLoaderData<typeof loader>();
+  const [theme] = useTheme();
+
   return (
-    <html lang="en">
+    <html lang="en" className={clsx(theme)}>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
+        <PreventFlashOnWrongTheme ssrTheme={Boolean(data.theme)} />
         <Links />
       </head>
-      <body>
-        {children}
+      <body className="text-slate-800 bg-white dark:text-slate-100 dark:bg-gray-900 h-dvh">
+        <Outlet />
         <ScrollRestoration />
         <Scripts />
       </body>
@@ -25,6 +47,15 @@ export function Layout({ children }: { children: React.ReactNode }) {
   );
 }
 
-export default function App() {
-  return <Outlet />;
+/**
+ * App wrapped by the ThemeProvider
+ */
+export default function AppWithThemeProvider() {
+  const data = useLoaderData<typeof loader>();
+
+  return (
+    <ThemeProvider specifiedTheme={data.theme} themeAction="/action/set-theme">
+      <App />
+    </ThemeProvider>
+  );
 }
