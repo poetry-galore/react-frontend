f6a95effc090342c3fc70c837331b27b90cbd094
Reagan Ronald Odhiambo <reaganronald491@gmail.com>
2024-07-24 01:25:53 +0300

    feat: profile avatar

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -7,6 +7,8 @@ import { ThemeToggle } from "~/components/theme-toggler";
 import { Button } from "~/components/ui/button";
 
 import { NavbarLogo } from "./logo";
+import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
+
 
 type NavbarProps = {
   loggedUser?: User | null;
@@ -17,7 +19,7 @@ type NavbarProps = {
  */
 export default function Navbar({ loggedUser }: NavbarProps) {
   return (
-    <div className="lg:w-2/3 mx-auto my-5 flex justify-between  bg-white h-20 dark:bg-dark">
+    <div className="lg:w-2/3 mx-auto my-5 flex justify-between  bg-dark h-20 dark:bg-dark">
       <div className="flex items-center ms-1 sm:ms-2 md:ms-4 lg:ms-0">
         <div className="w-1/3 sm:w-2/5 h-11/12 flex justify-start">
           <Link to="/">
@@ -29,7 +31,11 @@ export default function Navbar({ loggedUser }: NavbarProps) {
       <div className="flex justify-end items-center me-1 sm:me-2 md:me-4 lg:me-0">
         <ThemeToggle className="me-1 lg:me-4" />
         {loggedUser ? (
-          <Form method="post" action="/logout" className="group/logout">
+          <Form method="post" action="/logout" className="group/logout flex ">
+            <Avatar>
+              <AvatarImage src="https://github.com/shadcn.png" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
             <Button
               size={"default"}
               className="text-lg font-semibold border-none text-red-500 bg-white hover:bg-white hover:text-red-400 rounded-lg dark:text-red-500 dark:bg-inherit hover:dark:bg-inherit hover:dark:text-red-400"
