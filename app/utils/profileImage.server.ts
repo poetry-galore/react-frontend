b9ccfe3e0ccb54f7058b143e416468088a75e96f
Reagan Ronald Odhiambo <reaganronald491@gmail.com>
2024-07-31 23:58:07 +0300

    fix: added documentation to the functions

--- app/utils/profileImage.server.ts
+++ app/utils/profileImage.server.ts
@@ -1,22 +1,34 @@
 import sharp from 'sharp';
-import { trace } from 'potrace';
 import { parse } from 'svgson';
-import { string } from 'zod';
+import { vectorize } from '@neplex/vectorizer';
 
 
+
+/**
+ * Function to convert the profile image to an svg file
+ * 
+ * @param imageBuffer an image of any file type
+ * @returns a promise that reloves to the svg string or reject wit an error
+ */
 export async function convertImagetoSVG(imageBuffer: any): Promise<string> {
     return new Promise((resolve, reject) => {
         sharp(imageBuffer)
         .toFormat('png')
         .toBuffer()
-        .then((pngBuffer) => {
-            trace(pngBuffer, (err, svg) => {
-                if (err) return reject(err);
-                resolve(svg)
-            })
+        .then( async (pngBuffer) => {
+            const svgImage = await vectorize(pngBuffer);
+            if (!svgImage) reject({ error: 'Could not convert image'})
+            resolve(svgImage);
         }).catch(reject)
     })
 }
+
+/**
+ * A function that confirms if the file provided is an svg file
+ * 
+ * @param svgContent an svg string
+ * @returns true if validated and false if not
+ */
 export async function validateSVG(svgContent: string) {
     try {
         const parsedSVG = await parse(svgContent);
