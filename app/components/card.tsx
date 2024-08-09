7e11b353d19c664a352218cf8209bf671f10c6ad
Brian Kim <bryo.kim1@gmail.com>
2024-08-09 19:11:22 +0300

    update styling of the components

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -60,27 +60,26 @@ type PoemCardProps = {
  * Card for displaying poems.
  */
 export function PoemCard({ poem, children, className }: PoemCardProps) {
+  // Format the updated date to a better format. eg Thu, 8th Aug 2024
   const updatedAt = moment(poem.updatedAt).format("ddd, Do MMM YYYY");
 
   return (
     <Card
       className={cn(
-        "w-11/12 md:w-[750px] mb-10 p-5 rounded-xl border-none overflow-auto shadow shadow-slate-500 hover:bg-slate-200/90 dark:bg-dark dark:hover:bg-slate-800/50 dark:shadow-slate-700",
+        "w-11/12 md:w-[750px] mb-10 p-3 md:p-5 rounded-xl border-none overflow-auto shadow shadow-slate-500 hover:bg-slate-200/90 dark:bg-dark dark:hover:bg-slate-800/50 dark:shadow-slate-700",
         className,
       )}
     >
       {
         <CardHeader className="text-start p-1 mb-1">
-          <CardTitle className="text-3xl raleway mb-1 antialiased">
+          <CardTitle className="text-3xl raleway mb-0.5 md:mb-1 antialiased">
             <Link to={POEM_ROUTE(poem.id)} className="hover:text-primary">
               {poem.title}
             </Link>
           </CardTitle>
 
           <CardDescription className="ms-2 flex justify-start items-center raleway italic text-slate-400 dark:text-slate-600">
-            <div className="me-1 ">
-              <FeatherIcon className="h-4 w-4" />
-            </div>{" "}
+            <FeatherIcon className="h-4 w-4 me-1" />
             <Link
               to="#"
               className="italic text-base font-semibold hover:text-slate-700 hover:underline hover:underline-offset-2 dark:hover:text-slate-400"
@@ -91,7 +90,9 @@ export function PoemCard({ poem, children, className }: PoemCardProps) {
           </CardDescription>
         </CardHeader>
       }
-      <CardContent className="p-5 text-start text-lg">{children}</CardContent>
+      <CardContent className="md:p-5 text-start text-lg">
+        {children}
+      </CardContent>
 
       <CardFooter className="flex justify-end items-center p-0 italic text-sm text-slate-400 dark:text-slate-600">
         <CalendarClockIcon className="h-4 w-4 me-1" /> {updatedAt}
