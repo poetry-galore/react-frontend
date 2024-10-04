421a019d9945833baa22dc3b29918d6de2e07fff
bryokim <bryo.kim1@gmail.com>
2024-10-04 23:24:55 +0300

    Add title input and restyle the new poem page

--- app/routes/_poems.tsx
+++ app/routes/_poems.tsx
@@ -1,4 +1,15 @@
+import { defineConfig } from "@poetry-galore/rich-text-editor";
 import { Outlet } from "@remix-run/react";
+
+export const NEW_POEM_CONFIG_NAME = "new_poem_config";
+
+// Define Configurations for the editors used in the new and edit pages.
+defineConfig([
+  {
+    name: NEW_POEM_CONFIG_NAME,
+    config: { plugins: { toolbar: false, floatingMenu: true } },
+  },
+]);
 
 export default function PoemsLayout() {
   return (
